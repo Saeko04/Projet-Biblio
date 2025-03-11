bc2e8b53211d0ddfa45498c66864aa8ad50663ec
Saeko <louise.lemay04@gmail.com>
2025-03-11 11:56:21 +0100

    Add files via upload

---
+++ controleur/controleurPrincipal.php
@@ -0,0 +1,19 @@
+<?php
+
+function controleurPrincipal($action){
+    $lesActions = array();
+    $lesActions["defaut"] = "accueil.php";
+    $lesActions["chercher"] = "chercher.php";
+    $lesActions["contact"] = "contact.php";
+    $lesActions["livre"] = "livre.php";
+    
+    if (array_key_exists ( $action , $lesActions )){
+        return $lesActions[$action];
+    }
+    else{
+        return $lesActions["defaut"];
+    }
+
+}
+
+?>
