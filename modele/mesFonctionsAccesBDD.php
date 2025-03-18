9aa0e83df41c8c6f3d816fd6d87c3f26b26aee5a
Wubal <106257075+Wubal@users.noreply.github.com>
2025-03-18 11:39:14 +0100

    Update mesFonctionsAccesBDD.php
    Ajout de la fonction connect

---
+++ modele/mesFonctionsAccesBDD.php
@@ -0,0 +1,27 @@
+
+<?php
+
+// Ajout de la fonction connecte 
+function connect(){
+    $host = 'localhost';
+    $db   = 'admin';
+    $user = 'login4084';
+    $pass = 'kPdZlrHdSNQzGgW';
+    $charset = 'utf8mb4';
+
+    $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+
+    try {
+        $pdo = new PDO($dsn, $user, $pass);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        echo "Connexion à la base de données réussie !";
+        return $pdo;
+    }
+    catch (PDOException $e){
+        echo "Erreur de connexion : " . $e->getMessage();
+        exit;
+    }
+}
+
+
+?>
