ebe0fe84e26d3db0a6f08d36fba7e6871be9fb71
Ayyoub <77174228+Ayyoub-ch@users.noreply.github.com>
2025-03-18 13:37:03 +0100

    Update testLivres.php
    Correction du script de testLivres.php

--- tests/testLivres.php
+++ tests/testLivres.php
@@ -1,9 +1,11 @@
+<?php
 //script de test de la fonction getTousLesLivres
 
-include_once "modele/mesFonctionsAccesBDD.php"
+include_once "modele/mesFonctionsAccesBDD.php";
 $pdo=connect();
 
 // Appel de la fonction
 $livres = getTousLesLivres($pdo);
 
 var_dump($livres);
+?>
