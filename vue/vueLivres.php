4a2172d8c6cccfb60467827251b08734033f9ee0
Ayyoub <77174228+Ayyoub-ch@users.noreply.github.com>
2025-03-25 22:47:04 +0100

    Update vueLivres.php

--- vue/vueLivres.php
+++ vue/vueLivres.php
@@ -1,45 +1,32 @@
 <!DOCTYPE html>
 <html lang="fr">
 <head>
-  <meta charset="UTF-8">
-  <title>Connexion</title>
-  <link rel="stylesheet" type="text/css" href="../css/admin.css">
-  <style>
-    .message-container {
-      text-align: center;
-      margin-bottom: 20px;
-    }
-  </style>
+    <meta charset="UTF-8" />
+    <title>Liste des livres</title>
+    <link rel="stylesheet" type="text/css" href="../css/livre.css">
 </head>
 <body>
+    <h1>Voici l'ensemble des livres que l'on possède</h1>
 
-<div class="login-container">
-  <h2>Connexion</h2>
+    <div class="livre-container">
+        <?php
 
-  <div class="message-container">
-    <?php if (isset($message)): ?>
-        <p><?= htmlspecialchars($message) ?></p>
+        include_once __DIR__ . '/../modele/mesFonctionsAccesBDD.php';
+        $pdo = connect();
+        $livres = getTousLesLivres($pdo);
 
-        <?php if (isset($_SESSION['connected']) && $_SESSION['connected'] === true): ?>
-            <div class="redirection-container">
-                <p>Redirection en cours...</p>
-                <script>
-                  setTimeout(() => {
-                      window.location.href = '../index.php?action=membre';
-                  }, 2000);
-                </script>
-            </div>
-        <?php endif; ?>
+        foreach ($livres as $livre) {
+            echo "<div class='livre'>";
+            echo '<strong>Cotation :</strong> ' . $livre['cotation'] . '<br>';
+            echo '<strong>Titre :</strong> ' . $livre['titre'] . '<br>';
+            echo '<strong>Auteur :</strong> ' . $livre['auteur'] . '<br>';
+            echo '<strong>Résumé :</strong> ' . $livre['resume'] . '<br>';
+            echo '</div>';
+        }
 
-    <?php endif; ?>
-  </div>
+        disconnect($objpdo);
 
-  <form method="POST" action="">
-      <input type="text" name="username" placeholder="Nom d'utilisateur" required>
-      <input type="password" name="password" placeholder="Mot de passe" required>
-      <button type="submit">Se connecter</button>
-  </form>
-</div>
-
+        ?>
+    </div>
 </body>
 </html>
