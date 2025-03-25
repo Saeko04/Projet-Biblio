c21d3e92bb30e13743a2275ff7ebf00150e86c8c
Wubal <106257075+Wubal@users.noreply.github.com>
2025-03-25 10:45:31 +0100

    Update controleurPrincipal.php

--- controleur/controleurPrincipal.php
+++ controleur/controleurPrincipal.php
@@ -6,6 +6,8 @@ function controleurPrincipal($action){
     $lesActions["chercher"] = "chercher.php";
     $lesActions["accès"] = "acces.php";
     $lesActions["contact"] = "contact.php";
+    $lesActions["login"] = "login.php";
+    $lesActions["membre"] = "espaceMembre.php";
     
     if (array_key_exists ( $action , $lesActions )){
         return $lesActions[$action];
