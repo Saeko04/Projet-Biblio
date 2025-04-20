6db67fccc2fcf31382673f101715c07712b62d9d
Ayyoub <77174228+Ayyoub-ch@users.noreply.github.com>
2025-04-20 23:41:05 +0200

    Update livre.php
    remplissage du livre

--- controleur/livre.php
+++ controleur/livre.php
@@ -1,13 +1,8 @@
 <?php
 
-//  Partie d'appel au modèle si besoin 
+include_once __DIR__ . '/../modele/mesFonctionsAccesBDD.php';
+$pdo = connect();
+$livres = getTousLesLivres($pdo);
 
-
-// Partie de traitement des données récupérées si besoin pour mise à disposition de la vue
-
-
-
-
-// appel du script de vue qui permet de gerer l'affichage des donnees
-include "vue/vueContact.php";
+require __DIR__ . '/../vue/vuelivres.php';
 ?>
