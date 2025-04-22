4f28a69c7bc6a8579bb12901ca74612d96545550
Wubal <106257075+Wubal@users.noreply.github.com>
2025-04-22 17:20:44 +0200

    Update vuelogin.php

--- vue/vuelogin.php
+++ vue/vuelogin.php
@@ -3,43 +3,39 @@
 <head>
   <meta charset="UTF-8">
   <title>Connexion</title>
-  <link rel="stylesheet" type="text/css" href="../css/admin.css">
-  <style>
-    .message-container {
-      text-align: center;
-      margin-bottom: 20px;
-    }
-  </style>
+  <link rel="stylesheet" type="text/css" href="../css/login.css">
 </head>
 <body>
 
-<div class="login-container">
-  <h2>Connexion</h2>
+<div class="main-login-wrapper">
+  <div class="login-container">
+    <h2>Connexion</h2>
 
-  <div class="message-container">
-    <?php if (isset($message)): ?>
-        <p><?= htmlspecialchars($message) ?></p>
+    <div class="message-container">
+      <?php if (!empty($message)): ?>
+          <p class="message"><?= htmlspecialchars($message) ?></p>
 
-        <?php if (isset($_SESSION['connected']) && $_SESSION['connected'] === true): ?>
-            <div class="redirection-container">
-                <p>Redirection en cours...</p>
-                <script>
-                  setTimeout(() => {
-                      window.location.href = '../index.php?action=membre';
-                  }, 2000);
-                </script>
-            </div>
-        <?php endif; ?>
+          <?php if (!empty($_SESSION['connected']) && $_SESSION['connected'] === true): ?>
+              <div class="redirection-container">
+                  <p>Redirection en cours...</p>
+                  <script>
+                    setTimeout(() => {
+                        window.location.href = '../index.php?action=membre';
+                    }, 2000);
+                  </script>
+              </div>
+          <?php endif; ?>
+      <?php endif; ?>
+    </div>
 
-    <?php endif; ?>
+    <form method="POST" action="">
+        <input type="text" name="username" placeholder="Nom d'utilisateur" required>
+        <input type="password" name="password" placeholder="Mot de passe" required>
+        <button type="submit">Se connecter</button>
+    </form>
   </div>
-
-  <form method="POST" action="">
-      <input type="text" name="username" placeholder="Nom d'utilisateur" required>
-      <input type="password" name="password" placeholder="Mot de passe" required>
-      <button type="submit">Se connecter</button>
-  </form>
 </div>
+
 
 </body>
 </html>
