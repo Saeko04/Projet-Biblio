7d85fd6c69afeac7fcdad01c454a476ce07dcfdd
Ayyoub <77174228+Ayyoub-ch@users.noreply.github.com>
2025-05-13 21:02:57 +0200

    Update controleurPrincipal.php

--- controleur/controleurPrincipal.php
+++ controleur/controleurPrincipal.php
@@ -1,19 +1,25 @@
 <?php
 
-function controleurPrincipal($action){
+function controleurPrincipal($action)
+{
     $lesActions = array();
-    $lesActions["defaut"] = "accueil.php";
-    $lesActions["chercher"] = "chercher.php";
-    $lesActions["accès"] = "acces.php";
-    $lesActions["contact"] = "contact.php";
-    
-    if (array_key_exists ( $action , $lesActions )){
-        return $lesActions[$action];
-    }
-    else{
-        return $lesActions["defaut"];
-    }
+    $lesActions['defaut'] = 'accueil.php';
+    $lesActions['chercher'] = 'chercher.php';
+    $lesActions['livres'] = 'livres.php';
+    $lesActions['accès'] = 'acces.php';
+    $lesActions['contact'] = 'contact.php';
+    $lesActions['login'] = 'login.php';
+    $lesActions['membre'] = 'espaceMembre.php';
+    $lesActions['detail-livre'] = 'detailLivre.php';
+    $lesActions['modifierLivre'] = 'modifierLivre.php';
+    $lesActions['scanner'] = 'scannerLivre.php';
 
+
+    if (array_key_exists($action, $lesActions)) {
+        return $lesActions[$action];
+    } else {
+        return $lesActions['defaut'];
+    }
 }
 
 ?>
