3c6ab305d45961b0857d777e4bbd851323fb2fcd
Ayyoub <77174228+Ayyoub-ch@users.noreply.github.com>
2025-05-14 09:01:23 +0200

    Delete controleur directory

--- controleur/accueil.php
+++
@@ -1,6 +0,0 @@
-<?php
-// Ici vous pourriez charger un modèle ou récupérer des données
-// par exemple : $nouveautes = Livre::getNouveautes();
-
-// puis on appelle la vue
-include __DIR__ . "/../vue/vueAccueil.php";
